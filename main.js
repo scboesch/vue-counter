5445da28a2d612004ab0a346e383b3d34fcfa48c
Chris Boesch <ChrisBoesch@nus.edu.sg>
2019-09-14 17:06:12 +0800

    Added decrement button

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ var app = new Vue({
   methods: {
     increment: function() {
       this.counter = this.counter + 1;
-      //this.counter += 1;
+    },
+    decrement: function() {
+      this.counter = this.counter - 1;
     }
   }
 });
