bf1fee926a6fed55d485439aabbdc9d801348838
Chris Boesch <ChrisBoesch@nus.edu.sg>
2019-09-14 17:03:18 +0800

    Added increment button

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ var app = new Vue({
   el: "#app",
   data: {
     counter: 1
+  },
+  methods: {
+    increment: function() {
+      this.counter = this.counter + 1;
+      //this.counter += 1;
+    }
   }
 });
